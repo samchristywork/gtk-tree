e23af03825cb3c024edb447bd7e906d05e3eb353
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-21 21:24:25 -0500

    feat: Implement main function
    The main function now creates a window and shows some basic stuff in it,
    like a button and the drawing surface.

--- src/main.c
+++ src/main.c
@@ -89,3 +89,38 @@ static gboolean handle_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
   draw_tree(cr);
   return FALSE;
 }
+
+int main() {
+  tree = create_tree();
+  add_child(tree->root, create_node("child1"));
+  add_child(tree->root, create_node("child2"));
+  add_child(&tree->root->children[0], create_node("child3"));
+  add_child(&tree->root->children[0], create_node("child4"));
+  add_child(&tree->root->children[1], create_node("child5"));
+
+  serialize_tree(tree, "tree.txt");
+
+  gtk_init(NULL, NULL);
+
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Hello, World!");
+  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+  GtkWidget *container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  gtk_container_add(GTK_CONTAINER(window), container);
+
+  GtkWidget *button = gtk_button_new_with_label("Hello, World!");
+  gtk_container_add(GTK_CONTAINER(container), button);
+  g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+  GtkWidget *drawing_area = gtk_drawing_area_new();
+  gtk_container_add(GTK_CONTAINER(container), drawing_area);
+  gtk_widget_set_size_request(drawing_area, 800, 400);
+  g_signal_connect(drawing_area, "draw", G_CALLBACK(handle_draw), NULL);
+
+  g_signal_connect(window, "key-press-event", G_CALLBACK(handle_key), NULL);
+
+  gtk_widget_show_all(window);
+  gtk_main();
+}
