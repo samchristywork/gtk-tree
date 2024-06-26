f4ea72d380e006c8bf4c3e60d3e7e0fe0804681e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-25 21:11:03 -0500

    feat: Re-implement main function
    The previous `main` function had a bunch of hard-coded data in it. Now
    we actually load the data from a file.

--- src/main.c
+++ src/main.c
@@ -628,36 +628,29 @@ static gboolean handle_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
 }
 
 int main() {
-  tree = create_tree();
-  add_child(tree->root, create_node("child1"));
-  add_child(tree->root, create_node("child2"));
-  add_child(&tree->root->children[0], create_node("child3"));
-  add_child(&tree->root->children[0], create_node("child4"));
-  add_child(&tree->root->children[1], create_node("child5"));
+  Tree *tree = deserialize_tree("tree.txt");
 
-  serialize_tree(tree, "tree.txt");
+  draw_root = tree->root;
 
   gtk_init(NULL, NULL);
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(window), "Hello, World!");
-  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+  gtk_window_set_title(GTK_WINDOW(window), "Tree Editor");
+  gtk_window_set_default_size(GTK_WINDOW(window), 1600, 850);
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  GtkWidget *container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  GtkWidget *container = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
   gtk_container_add(GTK_CONTAINER(window), container);
 
-  GtkWidget *button = gtk_button_new_with_label("Hello, World!");
-  gtk_container_add(GTK_CONTAINER(container), button);
-  g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
-
-  GtkWidget *drawing_area = gtk_drawing_area_new();
+  drawing_area = gtk_drawing_area_new();
   gtk_container_add(GTK_CONTAINER(container), drawing_area);
-  gtk_widget_set_size_request(drawing_area, 800, 400);
+  gtk_widget_set_hexpand(drawing_area, TRUE);
+  gtk_widget_set_vexpand(drawing_area, TRUE);
   g_signal_connect(drawing_area, "draw", G_CALLBACK(handle_draw), NULL);
 
-  g_signal_connect(window, "key-press-event", G_CALLBACK(handle_key), NULL);
+  g_signal_connect(window, "key-press-event", G_CALLBACK(handle_key), tree);
 
   gtk_widget_show_all(window);
+
   gtk_main();
 }
